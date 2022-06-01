b6c82e21675d4f5d5a057daf5659a1df047cb36e
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-06-01 12:56:23 +0300

    create-react-app & tailwindcss setup

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          101: '#75cff0',
+          102: '#69B9D6',
+          103: '#5698B0',
+        },
+        yellow: {
+          101: '#f2d267'
+        },
+      },
+      fontFamily: {
+        sans: ['Ruda', 'sans-serif'],
+        serif: ['Roboto Slab', 'serif'],
+      },
+      backgroundImage: {},
+      height: {
+        '400': '400px',
+        '500': '500px',
+        '600': '600px',
+      }
+    },
+  },
+  plugins: [],
+}
