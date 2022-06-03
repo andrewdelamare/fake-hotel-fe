d8e31a374da6004bec9ed096d47136843227aa43
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-06-03 15:30:03 +0300

    add function to get all dates for month

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,4 +1,4 @@
-import { format, compareAsc, getMonth, getWeeksInMonth, getDaysInMonth, eachDayOfInterval, getYear } from "date-fns";
+import { format, compareAsc, getMonth, getWeeksInMonth, getDaysInMonth, eachDayOfInterval, getYear, startOfMonth, endOfMonth, parseISO } from "date-fns";
 import { startOfToday } from "date-fns";
 import { useState } from "react";
 
@@ -7,13 +7,27 @@ const Date = (updateDayStart, updateDayEnd, dateIndex) => {
 
 };
 
-const Month = (month) => {
-  const daysInMonth = getDaysInMonth(month.mon)
-  //const allDays = eachDayOfInterval() 
+const Month = ({month}) => {
+  const intYr = month.yr
+  const intMon = month.mon < 10 ? `0${month.mon}` : month.mon
+  const day = parseISO(`${intYr}-${intMon}-15`)
+  const start = startOfMonth(day)
+  const end = endOfMonth(day);
+ 
+  const allDays = eachDayOfInterval({
+    start: start, 
+    end: end
+  }) 
+  console.log(allDays)
   return (
   <div>
     <div className="grid grid-cols-7"> <div>S</div><div>M</div><div>T</div><div>W</div><div>T</div><div>F</div><div>S</div></div>
     <div className="grid grid-cols-7">
+      {
+        allDays.map(day => {
+          <div className="col-" >{day.getDay()}</div>
+        })
+      }
     </div>
   </div>  
   )
@@ -43,15 +57,15 @@ export const Calendar = () => {
   const updateMonth = (direction) => {
     switch (direction){
       case '+':
-        if (selectedMonth.mon === 11){
-          setSelectedMonth({mon: 12, yr: selectedMonth.yr});
-          setNextMonth({mon: 1, yr: nextMonth.yr + 1});
+        if (selectedMonth.mon === 10){
+          setSelectedMonth({mon: 11, yr: selectedMonth.yr});
+          setNextMonth({mon: 0, yr: nextMonth.yr + 1});
           console.log(selectedMonth)
           console.log(nextMonth);
           break
-        }else if (selectedMonth.mon === 12){
-          setSelectedMonth({mon: 1, yr: selectedMonth.yr + 1});
-          setNextMonth({mon: 2, yr: nextMonth.yr});
+        }else if (selectedMonth.mon === 11){
+          setSelectedMonth({mon: 0, yr: selectedMonth.yr + 1});
+          setNextMonth({mon: 1, yr: nextMonth.yr});
           console.log(selectedMonth)
           console.log(nextMonth);
           break
@@ -63,10 +77,25 @@ export const Calendar = () => {
           break;
         }   
       case '-': 
-        setSelectedMonth(selectedMonth - 1);
-        setNextMonth(nextMonth - 1);
-        console.log(nextMonth)
+      if (selectedMonth.mon === 0){
+        setSelectedMonth({mon: 11, yr: selectedMonth.yr - 1 });
+        setNextMonth({mon: 0, yr: nextMonth.yr});
+        console.log(selectedMonth)
+        console.log(nextMonth);
+        break
+      }else if (selectedMonth.mon === 11){
+        setSelectedMonth({mon: 10, yr: selectedMonth.yr});
+        setNextMonth({mon: 11, yr: nextMonth.yr - 1});
+        console.log(selectedMonth)
+        console.log(nextMonth);
+        break
+      }else{
+        setSelectedMonth({mon: selectedMonth.mon - 1, yr: selectedMonth.yr});
+        setNextMonth({mon: nextMonth.mon - 1, yr: nextMonth.yr});
+        console.log(selectedMonth)
+        console.log(nextMonth);
         break;
+      }   
       default:
         console.log("please enter + or - to change the month") 
         break;
