a599cf27c20ac2f09c8c0c74478e865fd9a91cd3
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-06-06 20:25:47 +0300

    add previous month and nextNext month values for transition usage

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -13,14 +13,13 @@ const Month = ({month}) => {
   const day = parseISO(`${intYr}-${intMon}-15`)
   const start = startOfMonth(day)
   const end = endOfMonth(day);
- 
+  console.log(intMon, intYr)
   const allDays = eachDayOfInterval({
     start: start, 
     end: end
   }) 
-  console.log(intMon)
   return (
-  <div className=" flex flex-col border-2 border-slate-900 rounded-xl px-5 ">
+  <div className="flex flex-col">
     <h1 className="self-center text-xl">{day.toLocaleString('default', { month: 'long', year: 'numeric' })}</h1>
     <div className="grid gap-3 grid-cols-7 pt-3 w-full"> 
       <div className="m-2">S</div>
@@ -68,6 +67,9 @@ export const Calendar = () => {
   const [selectedDayEnd, setSelectedDayEnd] = useState();
   const [selectedMonth, setSelectedMonth] = useState({mon: month, yr: year});
   const [nextMonth, setNextMonth] = useState({mon: month === 12 ? 1 : month + 1 , yr: month === 12 ? year + 1 : year});
+  const [nextNextMonth, setNextNextMonth] = useState({mon: month === 12 ? 2 : month === 11 ? 1 : month + 2 , yr: month === 12 ? year + 1 : month === 11 ? year + 1 : year});
+  const [lastMonth, setLastMonth] = useState({mon: month === 1 ? 12 : month - 1 , yr: month === 1 ? year - 1 : year});
+
 
   const updateDayStart = (date) => {
     setSelectedDayStart(date)
@@ -81,35 +83,66 @@ export const Calendar = () => {
     switch (direction){
       case '+':
         if (selectedMonth.mon === 11){
-          setSelectedMonth({mon: 12, yr: selectedMonth.yr});
+          setLastMonth({mon: selectedMonth.mon, yr: selectedMonth.yr})
+          setSelectedMonth({mon: selectedMonth.mon + 1, yr: selectedMonth.yr});
           setNextMonth({mon: 1, yr: nextMonth.yr + 1});
-          
+          setNextNextMonth({mon: nextNextMonth.mon + 1, yr: nextNextMonth.yr});
           break
         }else if (selectedMonth.mon === 12){
+          setLastMonth({mon: selectedMonth.mon, yr: selectedMonth.yr})
           setSelectedMonth({mon: 1, yr: selectedMonth.yr + 1});
           setNextMonth({mon: 2, yr: nextMonth.yr});
+          setNextNextMonth({mon: nextNextMonth.mon + 1, yr: nextNextMonth.yr});
+          
+          break
+        }else if (selectedMonth.mon === 10){
+          setLastMonth({mon: selectedMonth.mon, yr: selectedMonth.yr})
+          setSelectedMonth({mon: selectedMonth.mon + 1, yr: selectedMonth.yr});
+          setNextMonth({mon: nextMonth.mon + 1, yr: nextMonth.yr});
+          setNextNextMonth({mon: 1, yr: nextNextMonth.yr + 1});
           
           break
         }else{
+          setLastMonth({mon: selectedMonth.mon, yr: selectedMonth.yr})
           setSelectedMonth({mon: selectedMonth.mon + 1, yr: selectedMonth.yr});
           setNextMonth({mon: nextMonth.mon + 1, yr: nextMonth.yr});
-          
+          setNextNextMonth({mon: nextNextMonth.mon + 1, yr: nextNextMonth.yr});
           break;
         }   
       case '-': 
       if (selectedMonth.mon === 1){
+        setLastMonth({mon: lastMonth.mon - 1, yr: selectedMonth.yr})
+        setNextMonth({mon: selectedMonth.mon, yr: selectedMonth.yr});
+        setNextNextMonth({mon: nextNextMonth.mon - 1, yr: nextNextMonth.yr});
         setSelectedMonth({mon: 12, yr: selectedMonth.yr - 1 });
-        setNextMonth({mon: 1, yr: nextMonth.yr});
         break
-      }else if (selectedMonth.mon === 12){
-        setSelectedMonth({mon: 11, yr: selectedMonth.yr});
-        setNextMonth({mon: 12, yr: nextMonth.yr - 1});
+      }else if  (selectedMonth.mon === 2){
+        setLastMonth({mon: 12, yr: lastMonth.yr - 1})
+        setNextMonth({mon: selectedMonth.mon, yr: selectedMonth.yr});
+        setNextNextMonth({mon: nextNextMonth.mon - 1, yr: nextNextMonth.yr});
+        setSelectedMonth({mon: selectedMonth.mon - 1, yr: selectedMonth.yr});
         break
-      }else{
+      }else if  (selectedMonth.mon === 12){
+        setLastMonth({mon: lastMonth.mon - 1, yr: selectedMonth.yr})
+        setSelectedMonth({mon: selectedMonth.mon - 1, yr: selectedMonth.yr});
+        setNextMonth({mon: 12, yr: nextMonth.yr});
+        setNextNextMonth({mon: nextNextMonth.mon - 1, yr: nextNextMonth.yr});
+        break
+      }else if  (selectedMonth.mon === 11){
+        setLastMonth({mon: lastMonth.mon - 1, yr: selectedMonth.yr})
         setSelectedMonth({mon: selectedMonth.mon - 1, yr: selectedMonth.yr});
         setNextMonth({mon: nextMonth.mon - 1, yr: nextMonth.yr});
+        setNextNextMonth({mon: 12, yr: nextNextMonth.yr - 1});
         break;
-      }   
+      }else{
+        setLastMonth({mon: lastMonth.mon - 1, yr: selectedMonth.yr});
+        setNextMonth({mon: selectedMonth.mon, yr: selectedMonth.yr});
+        setNextNextMonth({mon: nextNextMonth.mon - 1, yr: nextNextMonth.yr});
+        setSelectedMonth({mon: selectedMonth.mon - 1, yr: selectedMonth.yr});
+        break;
+      }
+      break
+
       default:
         console.log("please enter + or - to change the month") 
         break;
@@ -124,9 +157,11 @@ export const Calendar = () => {
         <button className="mx-5" onClick={() => updateMonth('-')}>-</button>
         <button className="mx-5" onClick={() => updateMonth('+')}>+</button>
       </div>
-      <div className="flex-row mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mx-auto border-2 border-slate-900 rounded-xl px-5">
+        <Month month={lastMonth} />
         <Month month={selectedMonth} />
-        <Month month={nextMonth} />  
+        <Month month={nextMonth} />
+        <Month month={nextNextMonth} />  
       </div>
      
     </div>
