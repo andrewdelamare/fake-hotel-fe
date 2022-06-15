85001dbd92ae8a06f62cc60e4779a2a50a2ec672
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-06-15 12:32:24 +0300

    add amenity boxes to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -10,20 +10,65 @@
 //  Map with fake locaiton of hotel & points of interest around it
 //
 //  Footer
+import { Link } from "react-router-dom";
 import img from "../zero-take-WvHrrR1C5Po-unsplash.jpg";
+
+const LandingHero = () => (
+  <div className="relative">
+    <div className="">
+      <img src={img} alt="hotel" className="h-600 w-full object-cover" />
+    </div>
+
+    <div className="absolute top-1/2 left-1/4 right-1/4 text-white backdrop-blur-sm">
+      <div className="text-5xl">Central Finland Fake Hotel</div>
+    </div>
+  </div>
+);
+
+const Amenity = ({ title, desc, className, img, linkLoc }) => (
+  <div className={className + " relative place-content-center flex flex-col"}>
+    <Link
+      to={linkLoc}
+      className="w-auto h-full mr-4 mb-4  bg-slate-200 text-center place-content-center flex flex-col"
+    >
+      <div className="text-2xl">{title}</div>
+      <div>{desc}</div>
+    </Link>
+  </div>
+);
+
+const Amenities = () => (
+  <div className="block w-full h-auto pt-4 pl-4 pb-4 relative">
+    <Amenity
+      className="w-3/5 h-80 float-left relative"
+      title="Accomadations"
+      desc="Find your paradise"
+      linkLoc="/rooms"
+    />
+    <Amenity
+      className="w-2/5 h-96 float-right relative"
+      title="Gourmet Cuisine"
+      desc="World class service, locally inspired"
+      linkLoc="/"
+    />
+    <Amenity
+      className="w-3/5 h-64 float-left relative"
+      title="Meetings & Events"
+      linkLoc="/"
+    />
+    <Amenity
+      className="w-2/5 h-36 float-right relative"
+      title="Wellness Spa"
+      linkLoc="/"
+    />
+  </div>
+);
+
 export const Landing = () => {
   return (
-    <div className="relative">
-      <div className="">
-        <img src={img} alt="hotel" className="h-600 w-full object-cover" />
-      </div>
-
-      <div className="absolute top-1/2 left-1/4 right-1/4 text-white">
-        <div className="text-5xl">Central Finland Fake Hotel</div>
-        <div className="text-2xl">
-          Boutique luxury acomodation for the discerning traveler
-        </div>
-      </div>
+    <div>
+      <LandingHero />
+      <Amenities />
     </div>
   );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,7 +6,9 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <nav className="inline-flex justify-between sm:mr-4 sticky top-0 float-right w-full font-sans z-50">
-      <div className="pl-5 pt-3 mt-4 text-xl sm:text-2xl">Hotel Name</div>
+      <div className="pl-5 pt-3 mt-4 text-xl sm:text-2xl">
+        Central Finland Fake Hotel
+      </div>
       <div className="flex flex-row justify-between sm:mr-5 ">
         <Link
           to="/"
