eca477744b7772abf0e09ff6d38e1fe24d83f027
Andrew de Lamare <80968376+andrewdelamare@users.noreply.github.com>
2022-06-17 10:46:01 +0300

    highlight selected room in booking.js, add form styling

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -2,6 +2,18 @@
 // Book Your Stay With Us
 // Selector for options
 // Calendar with available dates for selected options
+// Continue Button which reveals form
+// Form with info and submit button
+// On submit either display success or failure
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 
@@ -14,6 +26,12 @@ import { Calendar } from "./Calendar";
 export const Booking = () => {
   const [rooms, setRooms] = useState([]);
   const [selectedRoom, setSelRoom] = useState(null);
+
+  const setSelStylez = (room) => {
+    return selectedRoom != null && room === selectedRoom.name
+      ? "bg-indigo-600"
+      : "";
+  };
 
   const chooseRoom = (room) => {
     setSelRoom(room);
@@ -67,18 +85,17 @@ export const Booking = () => {
     szSwitch();
   }
   return (
-    <div className="inline-flex">
+    <div className="inline-flex mt-5 mx-10 w-full">
       <div className="flex flex-col w-7/12 text-center items-center">
         <div className="text-xl pb-10">Select the dates for your stay</div>
         <Calendar />
-        <form className="flex flex-col text-left ">
+        <form className="ml-10 self-start flex flex-col text-left bg-slate-200 p-2 border-2 border-slate-900">
           <label className="block">
             <span className="block font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
               First Name
             </span>
             <input className="border-2"></input>
           </label>
-
           <label className="block">
             <span className="block font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
               Surname
@@ -86,10 +103,16 @@ export const Booking = () => {
             <input className="border-2"></input>
           </label>
           <label>
-            <span className="after:content-['*'] after:ml-0.5 after:text-red-500">
+            <span className="block font-medium after:content-['*'] after:ml-0.5 after:text-red-500">
+              Email address
+            </span>
+            <input className="border-2"></input>
+          </label>
+          <label>
+            <span className="block after:content-['*'] after:ml-0.5 after:text-red-500">
               Group size
             </span>
-            <select name="people">
+            <select name="people" className="bg-white p-1 mb-3">
               <option>Please select</option>
               <option value={1}>1</option>
               <option value={2}>2</option>
@@ -99,6 +122,12 @@ export const Booking = () => {
               <option value={6}>6</option>
             </select>
           </label>
+          <button
+            className="border-2 bg-indigo-400 rounded-xl w-1/2 self-center"
+            onSubmit={(event) => event.preventDefault()}
+          >
+            Submit
+          </button>
         </form>
       </div>
       <div className="flex flex-col w-5/12 text-center">
@@ -106,7 +135,9 @@ export const Booking = () => {
         <div className="flex flex-col h-full">
           {rooms.map((room) => (
             <button
-              className="p-10 border-2 border-slate-800 bg-indigo-400 z-10"
+              className={`m-5 p-5 border-2 rounded-xl w-2/3 self-center border-slate-800 bg-indigo-400 z-10 ${setSelStylez(
+                room.name
+              )}`}
               onClick={() => chooseRoom(room)}
               key={room.id}
             >
